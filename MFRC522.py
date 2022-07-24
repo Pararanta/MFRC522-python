a80367236dc4cbbd222c1400ee87cfc8fff687c2
Pararanta <54844502+Pararanta@users.noreply.github.com>
2022-07-24 21:11:31 +0200

    Update MFRC522.py

--- MFRC522.py
+++ MFRC522.py
@@ -370,10 +370,11 @@ class MFRC522:
     buff.append(crc[0])
     buff.append(crc[1])
     (status, backData, backLen) = self.MFRC522_ToCard(self.PCD_TRANSCEIVE, buff)
+    print(status)
+    print(backLen)
     if not(status == self.MI_OK) or not(backLen == 4) or not((backData[0] & 0x0F) == 0x0A):
         status = self.MI_ERR
     
-    print(backData);
     if status == self.MI_OK:
         i = 0
         buf = []
