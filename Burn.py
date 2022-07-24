c5134e839d170fb8e938dfc4df532af6b11c771c
Pararanta <54844502+Pararanta@users.noreply.github.com>
2022-07-24 21:03:18 +0200

    Update Burn.py

--- Burn.py
+++ Burn.py
@@ -1,11 +1,3 @@
-#!/usr/bin/env python
-from Crypto.PublicKey import ECC
-from Crypto.Hash import SHA256
-from Crypto.Signature import DSS
-import OPi.GPIO as GPIO
-import MFRC522
-import signal
-#!/usr/bin/env python
 from Crypto.PublicKey import ECC
 from Crypto.Hash import SHA256
 from Crypto.Signature import DSS
@@ -28,15 +20,16 @@ signal.signal(signal.SIGINT, end_read)
 # Create an object of the class MFRC522
 MIFAREReader = MFRC522.MFRC522()
 private_key = ECC.import_key(open('./keys/private_key.pem').read())
-signer = DSS.new(keypair, 'fips-186-3')
+signer = DSS.new(private_key, 'fips-186-3')
 # This loop keeps checking for chips. If one is near it will get the UID and authenticate
-key = [0x5c, 0xc7, 0x5d, 0xf8, 0x0c, 0x21]
+key = [0xff]*6
+new_key = [0x5c, 0xc7, 0x5d, 0xf8, 0x0c, 0x21]
 access_bits = [0x70, 0xf0, 0xf8, 0x00]
 while continue_reading:
-    
-    # Scan for cards    
+
+    # Scan for cards
     (status,TagType) = MIFAREReader.MFRC522_Request(MIFAREReader.PICC_REQIDL)
-    
+
     # Get the UID of the card
     (status,uid) = MIFAREReader.MFRC522_Anticoll()
 
@@ -58,9 +51,8 @@ while continue_reading:
             MIFAREReader.MFRC522_Write(5, Signature[32:48])
             MIFAREReader.MFRC522_Write(6, Signature[48:64])
 
-            for sector in range(16)
-                MIFAREReader.MFRC522_Write(sector*4 + 3, key + access_bits + key)
+            for sector in range(16):
+                MIFAREReader.MFRC522_Write(sector*4 + 3, new_key + access_bits + new_key)
             MIFAREReader.MFRC522_StopCrypto1()
             print('success')
 continue_reading = True
-
