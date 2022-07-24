3ffaa43acab1ab435f8d8878d2c0a1f70df20169
Pararanta <54844502+Pararanta@users.noreply.github.com>
2022-07-24 18:55:52 +0200

    Update Listen.py

--- Listen.py
+++ Listen.py
@@ -1,4 +1,7 @@
 #!/usr/bin/env python
+from Crypto.PublicKey import ECC
+from Crypto.Hash import SHA256
+from Crypto.Signature import DSS
 import OPi.GPIO as GPIO
 import MFRC522
 import signal
@@ -16,7 +19,8 @@ signal.signal(signal.SIGINT, end_read)
 
 # Create an object of the class MFRC522
 MIFAREReader = MFRC522.MFRC522()
-
+public_key = ECC.import_key(open('public_key.pem').read())
+verifier = DSS.new(public_key, 'fips-186-3')
 # This loop keeps checking for chips. If one is near it will get the UID and authenticate
 while continue_reading:
     
@@ -33,18 +37,21 @@ while continue_reading:
         print("{:02X}{:02X}{:02X}{:02X}".format(uid[0], uid[1], uid[2], uid[3]))
     
         # This is the default key for authentication
-        #key = [0xFF,0xFF,0xFF,0xFF,0xFF,0xFF]
-        
+        key = [0x5c, 0xc7, 0x5d, 0xf8, 0x0c, 0x21]
         # Select the scanned tag
-        #MIFAREReader.MFRC522_SelectTag(uid)
+        MIFAREReader.MFRC522_SelectTag(uid)
 
         # Authenticate
-        #status = MIFAREReader.MFRC522_Auth(MIFAREReader.PICC_AUTHENT1A, 8, key, uid)
+        status = MIFAREReader.MFRC522_Auth(MIFAREReader.PICC_AUTHENT1A, 8, key, uid)
 
         # Check if authenticated
-        #if status == MIFAREReader.MI_OK:
-        #    MIFAREReader.MFRC522_Read(8)
-        #    MIFAREReader.MFRC522_StopCrypto1()
-        #else:
-        #    print("Authentication error")
+        if status == MIFAREReader.MI_OK:
+            UUID = MIFAREReader.MFRC522_Read(0);
+            Signature = MIFAREReader.MFRC522_Read(1) + MIFAREReader.MFRC522_Read(2) + MIFAREReader.MFRC522_Read(5) + MIFAREReader.MFRC522_Read(6);
+            try:
+                verifier.verify(SHA256.new(bytes(i1)), bytes(i2))
+                print('verified ' + ''.join(format(x, '02x') for x in UUID))
+            except:
+                print('unverified ' + ''.join(format(x, '02x') for x in UUID))
+            MIFAREReader.MFRC522_StopCrypto1()
 
