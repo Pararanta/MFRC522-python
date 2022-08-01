050eedbd82978c491799c190a6752c5c85221d04
Pararanta <54844502+Pararanta@users.noreply.github.com>
2022-08-01 02:06:07 +0200

    Update Listen.py

--- Listen.py
+++ Listen.py
@@ -14,6 +14,9 @@ def end_read(signal,frame):
     continue_reading = False
     GPIO.cleanup()
 
+def toHex(number):
+    return "{:02X}".format(number)
+
 # Hook the SIGINT
 signal.signal(signal.SIGINT, end_read)
 
@@ -21,18 +24,32 @@ signal.signal(signal.SIGINT, end_read)
 MIFAREReader = MFRC522.MFRC522()
 public_key = ECC.import_key(open('./keys/public_key.pem').read())
 verifier = DSS.new(public_key, 'fips-186-3')
-# This loop keeps checking for chips. If one is near it will get the UID and authenticate
+key = [0xff, 0xff, 0xff, 0xff, 0xff, 0xff]
+# This loop keeps checking for chips. If one is near it will get the UID and aut
+henticate
 while continue_reading:
-    
-    # Scan for cards    
+
+    # Scan for cards
     (status,TagType) = MIFAREReader.MFRC522_Request(MIFAREReader.PICC_REQIDL)
-    
+
     # Get the UID of the card
     (status,uid) = MIFAREReader.MFRC522_Anticoll()
 
     # If we have the UID, continue
     if status == MIFAREReader.MI_OK:
-
         # Print UID
-        print("{:02X}{:02X}{:02X}{:02X}".format(uid[0], uid[1], uid[2], uid[3]))
+        data = []
+        MIFAREReader.MFRC522_SelectTag(uid)
+        for i in [8, 9, 10, 13, 14]:
 
+            status = MIFAREReader.MFRC522_Auth(MIFAREReader.PICC_AUTHENT1A, i, key, uid)
+            if status == MIFAREReader.MI_OK:
+                data = data + MIFAREReader.MFRC522_Read(i)
+
+            try:
+                verifier.verify(SHA256.new(bytes(data[0:16])), bytes(data[17, 80]))
+                print("verified " + "".join(map(toHex, uid + data[0:16])))
+            except:
+                print("unverified " + "".join(map(toHex, uid + data[0:16])))
+
+    MIFAREReader.MFRC522_StopCrypto1()
