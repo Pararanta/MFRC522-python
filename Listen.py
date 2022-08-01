1ae47ee2913ac59322a9d6b6cc7a93e4f5be324d
Pararanta <54844502+Pararanta@users.noreply.github.com>
2022-08-01 02:38:58 +0200

    Update Listen.py

--- Listen.py
+++ Listen.py
@@ -46,9 +46,9 @@ while continue_reading:
                 data = data + MIFAREReader.MFRC522_Read(i)
 
         print("checking " + "".join(map(toHex, uid + data[0:16])))
-        print("with " + "".join(map(toHex, uid + data[17:80])))
+        print("with " + "".join(map(toHex, uid + data[16:80])))
         try:
-            verifier.verify(SHA256.new(bytes(data[0:16])), bytes(data[17:80]))
+            verifier.verify(SHA256.new(bytes(data[0:16])), bytes(data[16:80]))
             print("verified " + "".join(map(toHex, uid + data[0:16])))
         except:
             print("unverified " + "".join(map(toHex, uid + data[0:16])))
