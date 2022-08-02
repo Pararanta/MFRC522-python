b43a8045baf7fce3174cd46cbc3b5e3f7a660286
leonhardk <konrad.leonhard@snpgroup.com>
2022-08-02 10:07:42 +0200

    Update Burn.py

--- Burn.py
+++ Burn.py
@@ -27,7 +27,7 @@ signer = DSS.new(private_key, 'fips-186-3')
 # This loop keeps checking for chips. If one is near it will get the UID and authenticate
 key = [0xff]*6
 new_key = [0x5c, 0xc7, 0x5d, 0xf8, 0x0c, 0x21]
-access_bits = [0x70, 0xf0, 0xf8, 0x00]
+access_bits = [0xFF, 0x07, 0x80, 0xFF]
 while continue_reading:
 
     # Scan for cards
